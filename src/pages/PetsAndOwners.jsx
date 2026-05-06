578185b969661e88b111488e52f084e9e510aea6
Helena Rhawi <helena.rhawi@live.com>
2026-05-06 20:16:04 +0200

    added useState and use Éffect to fetch json content

--- src/pages/PetsAndOwners.jsx
+++ src/pages/PetsAndOwners.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Lorem from "../parts/Lorem";
 import HeroImage from "../parts/HeroImage";
 
@@ -8,15 +9,40 @@ PetsAndOwners.route = {
 };
 
 export default function PetsAndOwners() {
+  const [pets, setPets] = useState();
+
+  useEffect(() => {
+    (async () => {
+      // fetch data from the url /pets.json
+      const response = await fetch("/json/pets.json");
+      //deserialize/unpack the json from to an array of objects
+      const data = await response.json();
+      //update the state variable pets
+      setPets(data);
+    })();
+  }, []);
+
+  console.log(pets);
   return (
-    <>
-      <HeroImage
-        src="dog-and-owner.webp"
-        alt="A dog and its owner"
-        heading="Pets & owners"
-      />
-      <p>This page will show pets and owners.</p>
-      <Lorem paragraphCount={3} />
-    </>
+    pets && (
+      <>
+        <HeroImage
+          src="dog-and-owner.webp"
+          alt="A dog and its owner"
+          heading="Pets & owners"
+        />
+        <h3>Pets</h3>
+        <section className="pets">
+          {pets.map(({ id, name, species }) => (
+            <div key={id}>
+              <h4>{name}</h4>
+              <p>
+                {name} is a {species},
+              </p>
+            </div>
+          ))}
+        </section>
+      </>
+    )
   );
 }
